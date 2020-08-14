f06d089d471043a1a9c361a6c40d3d67434d6147
jlmortola <jlmortola@me.com>
2020-08-14 12:10:43 +0200

    initial commit

---
+++ babel.config.js
@@ -0,0 +1,18 @@
+module.exports = (api) => {
+  api.cache(true);
+  return {
+    presets: ['babel-preset-expo'],
+    plugins: [
+      [
+        'module-resolver',
+        {
+          alias: {
+            'screens': './src/screens',
+            'components': './src/components',
+            'utils': './src/utils',
+          },
+        },
+      ],
+    ],
+  };
+};
